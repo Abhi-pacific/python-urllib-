0e8af9511f75278cdc9021b6077892943df31de6
Pacific <abhishek28c@gmail.com>
2023-06-26 22:31:19 +0530

    changes

--- openurl.py
+++ openurl.py
@@ -1,6 +1,11 @@
-import urllib.request
+import urllib.request, urllib.parse
+from bs4 import BeautifulSoup
 counts = dict()
-file = urllib.request.urlopen('https://www.google.com/')
-for data in file:
-    counts[data] = counts.get(data,0)+1
+file = urllib.request.urlopen('http://www.dr-chuck.com/page1.html').read()
+soup = BeautifulSoup(file,'file.parser')
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href',None))
+#for data in file:
+ #   counts[data] = counts.get(data,0)+1
 print(counts)
