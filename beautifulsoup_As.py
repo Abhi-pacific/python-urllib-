da6fa6e40061fa10a3a2115c232306715e43f8b9
Pacific <abhishek28c@gmail.com>
2023-06-27 15:21:24 +0530

    final task done

--- beautifulsoup_As.py
+++ beautifulsoup_As.py
@@ -1,18 +1,22 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-def scraping(url, file):
-    r = requests.get(url)
-    with open (file,'w') as f:
-        f.write(r.text)
-    with open(file,'r') as f:
-        data = f.read()
-    a = BeautifulSoup(data,'html.parser')
-    mass = a.find_all('td')
-    print(re.findall('[0-9]+',mass))
 
-    
+def scraping(url,file):
+    sum1 = 0
+    r = requests.get(url)
+    with open (file,'w') as r1:
+        r1.write(str(r.text))
+    with open (file,'r') as r2:
+        mass = r2.read()
+        soup = BeautifulSoup(mass,'html.parser')
+        max1 = re.findall('[0-9]+',str(soup('span')))
+        for max in max1:
+            sum1 += int(max)
+        print(sum1)
+            
+
 def main():
     url = 'http://py4e-data.dr-chuck.net/comments_1738511.html'
-    scraping(url, 'D:/python-urllib-/data.html')
+    scraping(url,'D:/python-urllib-/data.html')
 main()
